37b5e9b5537e3a32c812cc9dafd8e933dd4f0dd9
suksakk <kr.sak999@gmail.com>
2024-10-14 20:07:41 +0900

    board change

--- src/main/java/com/study/domain/post/PostService.java
+++ src/main/java/com/study/domain/post/PostService.java
@@ -20,84 +20,79 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor // 모든 필드에 생성자 자동 생성
 public class PostService {
 
-  private final PostMapper postMapper; // 이전 글에서 처리한 게시글 CRUD 기능을 포함하고 있는 Mapper 인터페이스
+	private final PostMapper postMapper; // 이전 글에서 처리한 게시글 CRUD 기능을 포함하고 있는 Mapper 인터페이스
 
-  @Transactional
-  public void viewCnt(Long id) {
-    postMapper.viewCnt(id);
-  }
+	@Transactional
+	public void viewCnt(Long id) {
+		postMapper.viewCnt(id);
+	}
 
-  /**
-   * 게시글 저장
-   * 
-   * @param params - 게시글 정보
-   * @return Generated PK
-   */
-  // 스프링에서 제공하는 트랜잭션 처리방법, "선언적 트랜잭션으로 불림 호출된 메서드에 해당 어노테이션이 선언되어 있으면 메서드의 실행과 동시에
-  // 트랜잭션 시작,
-  // 메서드의 정상 종료 여부에 따라 Commit 또는 Rollback 됩니다.
-  @jakarta.transaction.Transactional
-  public Long savePost(final PostRequest params) { // savePost() 게시글 생성, INSERT 완료시 생성된 게시글 id 리턴
-    postMapper.save(params); // postMapper의 save() 함수 호출
-    return params.getId(); // 생성된 게시글 ID가 null이 되지 않기 위해서는 PK값을 객체에 담아줘야한다 이것을 MyBatis의 "useGeneratedKeys"
-                           // 기능 이용 --> PostMapper.xml 이동
-  }
+	/**
+	 * 게시글 저장
+	 * @param params - 게시글 정보
+	 * @return Generated PK
+	 */
+	// 스프링에서 제공하는 트랜잭션 처리방법, "선언적 트랜잭션으로 불림 호출된 메서드에 해당 어노테이션이 선언되어 있으면 메서드의 실행과 동시에
+	// 트랜잭션 시작,
+	// 메서드의 정상 종료 여부에 따라 Commit 또는 Rollback 됩니다.
+	@jakarta.transaction.Transactional
+	public Long savePost(final PostRequest params) { // savePost() 게시글 생성, INSERT 완료시 생성된 게시글 id 리턴
+		postMapper.save(params); // postMapper의 save() 함수 호출
+		return params.getId(); // 생성된 게시글 ID가 null이 되지 않기 위해서는 PK값을 객체에 담아줘야한다 이것을 MyBatis의 "useGeneratedKeys"
+		// 기능 이용 --> PostMapper.xml 이동
+	}
 
-  /**
-   * 게시글 상세정보 조회
-   * 
-   * @param id - PK
-   * @return 게시글 상세정보
-   */
-  public PostResponse findPostById(final Long id) { // findById() 특정 게시글의 상세정보 조회
-    postMapper.findById(id);
-    return postMapper.findById(id);
-  }
+	/**
+	 * 게시글 상세정보 조회
+	 * @param id - PK
+	 * @return 게시글 상세정보
+	 */
+	public PostResponse findPostById(final Long id) { // findById() 특정 게시글의 상세정보 조회
+		postMapper.findById(id);
+		return postMapper.findById(id);
+	}
 
-  /**
-   * 게시글 수정
-   * 
-   * @param params - 게시글 정보
-   * @return PK
-   */
-  @Transactional
-  public Long updatePost(final PostRequest params) { // updatePost() 게시글의 수정 UPDATE 완료 시, 게시글 id 리턴
-    postMapper.update(params);
-    return params.getId();
-  }
+	/**
+	 * 게시글 수정
+	 * @param params - 게시글 정보
+	 * @return PK
+	 */
+	@Transactional
+	public Long updatePost(final PostRequest params) { // updatePost() 게시글의 수정 UPDATE 완료 시, 게시글 id 리턴
+		postMapper.update(params);
+		return params.getId();
+	}
 
-  /**
-   * 게시글 삭제
-   * 
-   * @param id - PK
-   * @return PK
-   */
-  public Long deletePost(final Long id) { // deletePost() 게시글 id에 따라 게시글 삭제 게시글 id 리턴
-    postMapper.deleteById(id);
-    return id;
-  }
+	/**
+	 * 게시글 삭제
+	 * @param id - PK
+	 * @return PK
+	 */
+	public Long deletePost(final Long id) { // deletePost() 게시글 id에 따라 게시글 삭제 게시글 id 리턴
+		postMapper.deleteById(id);
+		return id;
+	}
 
-  /**
-   * 게시글 리스트 조회
-   * 
-   * @param params - search conditions
-   * @return list & pagination information
-   */
-  public PagingResponse<PostResponse> findAllPost(final SearchDto params) {
+	/**
+	 * 게시글 리스트 조회
+	 * @param params - search conditions
+	 * @return list & pagination information
+	 */
+	public PagingResponse<PostResponse> findAllPost(final SearchDto params) {
 
-    // 조건에 해당하는 데이터가 없는 경우, 응답 데이터에 비어있는 리스트와 null을 담아 반환
-    int count = postMapper.count(params);
-    if (count < 1) {
-      return new PagingResponse<>(Collections.emptyList(), null);
-    }
+		// 조건에 해당하는 데이터가 없는 경우, 응답 데이터에 비어있는 리스트와 null을 담아 반환
+		int count = postMapper.count(params);
+		if (count < 1) {
+			return new PagingResponse<>(Collections.emptyList(), null);
+		}
 
-    // Pagination 객체를 생성해서 페이지 정보 계산 후 SearchDto 타입의 객체인 params에 계산된 페이지 정보 저장
-    Pagination pagination = new Pagination(count, params);
-    params.setPagination(pagination);
+		// Pagination 객체를 생성해서 페이지 정보 계산 후 SearchDto 타입의 객체인 params에 계산된 페이지 정보 저장
+		Pagination pagination = new Pagination(count, params);
+		params.setPagination(pagination);
 
-    // 계산된 페이지 정보의 일부(limitStart, recordSize)를 기준으로 리스트 데이터 조회 후 응답 데이터 반환
-    List<PostResponse> list = postMapper.findAll(params);
-    return new PagingResponse<>(list, pagination);
-  }
+		// 계산된 페이지 정보의 일부(limitStart, recordSize)를 기준으로 리스트 데이터 조회 후 응답 데이터 반환
+		List<PostResponse> list = postMapper.findAll(params);
+		return new PagingResponse<>(list, pagination);
+	}
 
 }
